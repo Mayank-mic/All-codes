96895443c609fc5e59e6cf62b97f6ff89554621f
GigaChad <84654653+Mayank-mic@users.noreply.github.com>
2023-07-17 09:34:29 +0530

    Update LC - (E) - Minimum Index Sum of Two Lists.cpp

--- LC - (E) - Minimum Index Sum of Two Lists.cpp
+++ LC - (E) - Minimum Index Sum of Two Lists.cpp
@@ -8,36 +8,33 @@
 // Return all the common strings with the least index sum. Return the answer in any order.
 
 
-
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
         
-        unordered_map<string , vector<int>> mp;
+        unordered_map<string , int> mp;
 
         for(int i=0;i<list1.size();++i){
-            mp[list1[i]].push_back(i);
-            mp[list1[i]].push_back(0);
+            mp[list1[i]]=i;
         }
 
         int mini = INT_MAX;
+         vector<string> ans;
 
         for(int j=0;j<list2.size();++j){
 
             if(mp.find(list2[j])!= mp.end()){
-                mp[list2[j]][0]+=j;
-                mp[list2[j]][1]=1;
-                mini =min(mp[list2[j]][0], mini);
+                mp[list2[j]]+=j;
+
+                if(mp[list2[j]] < mini){
+                    mini = mp[list2[j]];
+                    ans.clear();
+                    ans.push_back(list2[j]);
+                }else if(mini == mp[list2[j]]){
+                    ans.push_back(list2[j]);
+                }
             }
 
-        }
-
-        vector<string> ans;
-
-        for(int i=0;i<list1.size();++i){
-            if(mp[list1[i]][0]==mini && mp[list1[i]][1]==1){
-                ans.push_back(list1[i]);
-            }
         }
 
         return ans;
