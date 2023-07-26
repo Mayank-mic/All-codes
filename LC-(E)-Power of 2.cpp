c5824523904c272ecee3b4562713ca38d31a97a4
GigaChad <84654653+Mayank-mic@users.noreply.github.com>
2023-07-26 13:55:03 +0530

    Create LC-(E)-Power of 2.cpp

---
+++ LC-(E)-Power of 2.cpp
@@ -0,0 +1,13 @@
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+
+         return n > 0 && !(n & (n-1));
+        
+    }
+};
