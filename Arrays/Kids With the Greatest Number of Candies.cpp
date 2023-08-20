a99b498f1b8e3bf9e3cdbccd188783feec93434a
GigaChad <84654653+Mayank-mic@users.noreply.github.com>
2023-08-20 16:03:15 +0530

    Create Kids With the Greatest Number of Candies.cpp

---
+++ Arrays/Kids With the Greatest Number of Candies.cpp
@@ -0,0 +1,32 @@
+// There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+// Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+// Note that multiple kids can have the greatest number of candies.
+
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+
+
+        vector<bool> ans;
+
+
+        int n = candies.size();
+        int maxi=INT_MIN;
+
+        for(int i=0;i<n;++i){
+            maxi=max(maxi, candies[i]);
+        }
+
+
+        for(int i=0;i<n;++i){
+            if((candies[i]+extraCandies)>=maxi) ans.push_back(true);
+            else ans.push_back(false);
+        }
+
+        return ans;
+        
+    }
+};
