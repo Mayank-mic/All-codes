b943c723ccc16ca7a85abe9e6b22d5ffa5b7587a
GigaChad <84654653+Mayank-mic@users.noreply.github.com>
2023-08-22 18:04:05 +0530

    Create DSU default.cpp

---
+++ DSU/DSU default.cpp
@@ -0,0 +1,40 @@
+
+class dsu{
+
+
+    public:
+    vector<int> parent;
+    vector<int> size;
+
+    dsu(int n){
+        parent.resize(n+1);
+        size.resize(n+1, 1);
+
+
+        for(int i=0;i<=n;++i){
+            parent[i]=i;
+        }
+    }
+
+
+    int findUPar(int node){
+        if(parent[node]==node) return;
+        return parent[node]=findUPar(parent[node]);
+    }
+
+
+    void ubs(int u, int v){
+        int ulp_u = parent[u];
+        int ulp_v = parent[v];
+
+        if(ulp_v == ulp_u) return;
+        else if(size[ulp_u]>size[ulp_v]){
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }else{
+            parent[ulp_u] = ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+
+    }
+};
