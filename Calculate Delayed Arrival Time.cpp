579cbada766969f8f78369dde1b521203898e050
GigaChad <84654653+Mayank-mic@users.noreply.github.com>
2023-09-19 17:39:01 +0530

    Create Calculate Delayed Arrival Time.cpp

---
+++ Calculate Delayed Arrival Time.cpp
@@ -0,0 +1,16 @@
+// You are given a positive integer arrivalTime denoting the arrival time of a train in hours, and another positive integer delayedTime denoting the amount of delay in hours.
+
+// Return the time when the train will arrive at the station.
+
+// Note that the time in this problem is in 24-hours format.
+
+
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+
+
+        return  ( arrivalTime+delayedTime )%24;
+        
+    }
+};
