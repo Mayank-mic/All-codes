604f7f4ab683b2399e450e955c37ab6f45d3884d
GigaChad <84654653+Mayank-mic@users.noreply.github.com>
2023-09-21 16:52:43 +0530

    Create Group the People Given the Group Size They Belong To.cpp

---
+++ Arrays/Group the People Given the Group Size They Belong To.cpp
@@ -0,0 +1,45 @@
+// // There are n people that are split into some unknown number of groups. Each person is labeled with a unique ID from 0 to n - 1.
+
+// // You are given an integer array groupSizes, where groupSizes[i] is the size of the group that person i is in. For example, if groupSizes[1] = 3, then person 1 must be in a group of size 3.
+
+// // Return a list of groups such that each person i is in a group of size groupSizes[i].
+
+// // Each person should appear in exactly one group, and every person must be in a group. If there are multiple answers, return any of them. 
+// It is guaranteed that there will be at least one valid solution for the given input.
+
+
+
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+
+        unordered_map<int, vector<int>> mp;
+
+        for(int i=0;i<groupSizes.size();++i){
+            mp[groupSizes[i]].push_back(i);
+        }
+
+
+        vector<vector<int>> ans;
+
+        for(auto it: mp){
+            int p=0;
+            while(p!=it.second.size()){
+
+                vector<int> temp;
+
+                while(temp.size()!=it.first){
+                    temp.push_back(it.second[p]);
+                    p++;
+                }
+
+                ans.push_back(temp);
+
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+};
